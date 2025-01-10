79bf64a2bc1ddbef520752dfc459fe55a5698da4
devdaradara <devdaradara@gmail.com>
2025-01-10 14:35:33 +0900

    refactor: API 응답 형식 통일화

--- src/main/java/com/example/starhub/controller/CommentController.java
+++ src/main/java/com/example/starhub/controller/CommentController.java
@@ -4,41 +4,42 @@ import com.example.starhub.code.ResponseCode;
 import com.example.starhub.dto.comment.CommentRequestDto;
 import com.example.starhub.dto.comment.CommentResponseDto;
 import com.example.starhub.dto.response.ResponseDTO;
-import com.example.starhub.entity.CommentEntity;
 import com.example.starhub.projection.comment.GetCommentList;
 import com.example.starhub.repository.PostRepository;
 import com.example.starhub.repository.UserRepository;
 import com.example.starhub.service.CommentService;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/comment")
 public class CommentController {
     private final CommentService commentService;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
 
-    public CommentController(CommentService commentService, UserRepository userRepository, PostRepository postRepository) {
-        this.commentService = commentService;
-        this.userRepository = userRepository;
-        this.postRepository=postRepository;
-    }
-
     @PostMapping("/create")
-    public CommentResponseDto createComment(@RequestBody CommentRequestDto requestDto) {
-        return commentService.createComment(requestDto);
+    public ResponseEntity<ResponseDTO> createComment(@RequestBody CommentRequestDto requestDto) {
+        CommentResponseDto res = commentService.createComment(requestDto);
+
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_CREATE_COMMENT.getStatus().value())
+                .body(new ResponseDTO(ResponseCode.SUCCESS_CREATE_COMMENT, res));
     }
 
     // 댓글 목록 조회
     //쿼리변수
     @GetMapping("/list")
-    public List<CommentResponseDto> readAllComments(@RequestParam("post_id") Integer post_id) {
-        return commentService.readAllComments(post_id);
+    public ResponseEntity<ResponseDTO> readAllComments(@RequestParam("post_id") Integer post_id) {
+        List<CommentResponseDto> res = commentService.readAllComments(post_id);
+
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_COMMENTS.getStatus().value())
+                .body(new ResponseDTO(ResponseCode.SUCCESS_GET_COMMENTS, res));
     }
 
     @PutMapping("/pick")
@@ -51,12 +52,12 @@ public class CommentController {
     }
 
     @GetMapping("/pick/list")
-    public ResponseEntity<List<GetCommentList>> getPickedComments(@RequestParam("postId") Integer postId) {
+    public ResponseEntity<ResponseDTO> getPickedComments(@RequestParam("postId") Integer postId) {
         // CommentService를 사용하여 픽한 댓글 정보를 가져오는 메서드 호출
-        List<GetCommentList> pickedComments = commentService.getPickedComments(postId);
+        List<GetCommentList> res = commentService.getPickedComments(postId);
 
-        // ResponseEntity를 사용하여 응답 생성
-        return new ResponseEntity<>(pickedComments, HttpStatus.OK);
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_PICKED_COMMENTS.getStatus().value())
+                .body(new ResponseDTO(ResponseCode.SUCCESS_GET_PICKED_COMMENTS, res));
     }
-
 }
--- src/main/java/com/example/starhub/controller/PostController.java
+++ src/main/java/com/example/starhub/controller/PostController.java
@@ -1,32 +1,42 @@
 package com.example.starhub.controller;
 
+import com.example.starhub.code.ResponseCode;
 import com.example.starhub.dto.post.PostListResponseDto;
 import com.example.starhub.dto.post.PostRequestDto;
 import com.example.starhub.dto.post.PostResponseDto;
+import com.example.starhub.dto.response.ResponseDTO;
 import com.example.starhub.repository.UserRepository;
 import com.example.starhub.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api/post")
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
 public class PostController {
     private final PostService postService;
-
     private final UserRepository userRepository;
 
-    public PostController(PostService postService, UserRepository userRepository){
-        this.postService=postService;
-        this.userRepository=userRepository;
-    }
     @PostMapping("/create")
-    public PostResponseDto createPost(@RequestBody PostRequestDto requestDto){
-        return postService.createPost(requestDto);
+    public ResponseEntity<ResponseDTO> createPost(@RequestBody PostRequestDto requestDto) {
+        PostResponseDto res = postService.createPost(requestDto);
+
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_CREATE_POST.getStatus().value())
+                .body(new ResponseDTO(ResponseCode.SUCCESS_CREATE_POST, res));
     }
 
     @GetMapping("/list")
-    public List<PostListResponseDto> getAllPosts() {return postService.findAllPost();}
+    public ResponseEntity<ResponseDTO> getAllPosts() {
+        List<PostListResponseDto> res = postService.findAllPost();
+
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_POSTS.getStatus().value())
+                .body(new ResponseDTO(ResponseCode.SUCCESS_GET_POSTS, res));
+    }
 
     //경로변수
 //    @GetMapping("/detail/{post_id}")
@@ -36,7 +46,11 @@ public class PostController {
 
     //쿼리변수
     @GetMapping("/detail")
-    public PostResponseDto getOnePost(@RequestParam("post_id") Integer post_id) {
-        return postService.findOnePost(post_id);
+    public ResponseEntity<ResponseDTO> getOnePost(@RequestParam("post_id") Integer post_id) {
+        PostResponseDto res = postService.findOnePost(post_id);
+
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_POST.getStatus().value())
+                .body(new ResponseDTO(ResponseCode.SUCCESS_GET_POST, res));
     }
 }
