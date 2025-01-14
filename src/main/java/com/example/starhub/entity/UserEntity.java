4dfd95d0d72a5fcac96cf586c0c2635aaf431568
pyounani <yndbsk9372@gmail.com>
2025-01-14 13:20:43 +0900

    [Comment] 엔티티 클래스에 주석 추가 #2

--- src/main/java/com/example/starhub/entity/UserEntity.java
+++ src/main/java/com/example/starhub/entity/UserEntity.java
@@ -17,39 +17,39 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; // 유저 고유 식별자
 
     @Column(nullable = false, unique = true, length = 50)
     private String username; // 사용자 아이디
 
     @Column(nullable = false)
-    private String password;
+    private String password; // 비밀번호
 
     @Column(length = 255)
-    private String profileImage;
+    private String profileImage; // 프로필 이미지
 
     @Column(length = 50)
-    private String nickname;
+    private String nickname; // 닉네임
 
     @Column(length = 50)
     private String name; // 사용자 이름
 
-    private Integer age;
+    private Integer age; // 나이
 
     @Column(length = 255)
-    private String bio;
+    private String bio; // 한 줄 소개
 
     @Column(length = 100)
-    private String email;
+    private String email; // 이메일
 
     @Column(length = 15)
-    private String phoneNumber;
+    private String phoneNumber; // 전화번호
 
     @Column(nullable = false, updatable = false)
-    private LocalDateTime created_at;
+    private LocalDateTime created_at; // 생성일
 
     @Column(nullable = false)
-    private LocalDateTime updated_at;
+    private LocalDateTime updated_at; // 수정일
 
-    private Boolean isProfileComplete;
+    private Boolean isProfileComplete; // 프로필 생성 여부
 }
