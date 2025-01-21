82ddb4d0ab0b5611fb79d3b3592e0ea4da3d04b6
pyounani <yndbsk9372@gmail.com>
2025-01-21 19:55:28 +0900

    [Feat] 유효성 검사 로직 추가 #19

--- src/main/java/com/example/starhub/dto/request/CreateUserRequestDto.java
+++ src/main/java/com/example/starhub/dto/request/CreateUserRequestDto.java
@@ -3,13 +3,22 @@ package com.example.starhub.dto.request;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 public class CreateUserRequestDto {
 
-    @NotBlank(message = "사용자명을 입력해주세요.")
+    @NotBlank(message = "아이디를 입력해주세요.")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9-_]{6,12}$",
+            message = "아이디는 6~12자 영문, 숫자, 기호(-, _)만 사용 가능합니다."
+    )
     private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*[0-9]|.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,20}$",
+            message = "비밀번호는 8~20자 이내로 숫자, 특수문자, 영문자 중 2가지 이상 조합이어야 합니다."
+    )
     private String password;
 }
