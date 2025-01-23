5f61110a5efd25e28651d45c9bff9508fd6402a2
pyounani <yndbsk9372@gmail.com>
2025-01-23 11:26:52 +0900

    [Feat] DTO에 validation 추가 #28

--- src/main/java/com/example/starhub/controller/TechStackController.java
+++ src/main/java/com/example/starhub/controller/TechStackController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -36,7 +37,7 @@ public class TechStackController implements TechStackControllerDocs {
      */
     @Hidden
     @PostMapping
-    public ResponseEntity<ResponseDto> createTechStack(@RequestBody CreateTechStackRequestDto createTechStackRequestDto) {
+    public ResponseEntity<ResponseDto> createTechStack(@Valid @RequestBody CreateTechStackRequestDto createTechStackRequestDto) {
         techStackService.createTechStack(createTechStackRequestDto.getTechStacks());
         return ResponseEntity
                 .status(ResponseCode.SUCCESS_CREATE_TECH_STACK.getStatus().value())
