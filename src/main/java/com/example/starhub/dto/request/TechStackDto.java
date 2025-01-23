7a606bc25889a08e48ec214bee70e044c9f1cb86
pyounani <yndbsk9372@gmail.com>
2025-01-23 11:24:10 +0900

    [Feat] DTO에 validation 추가 #28

--- src/main/java/com/example/starhub/dto/request/TechStackDto.java
+++ src/main/java/com/example/starhub/dto/request/TechStackDto.java
@@ -3,9 +3,17 @@ package com.example.starhub.dto.request;
 import com.example.starhub.entity.enums.TechCategory;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 public class TechStackDto {
-    private String name;
-    private TechCategory category;
 
+    @NotBlank(message = "기술 스택 이름은 필수입니다")
+    @Size(max = 100, message = "기술 스택 이름은 100자를 초과할 수 없습니다")
+    private String name;
+
+    @NotNull(message = "카테고리는 필수입니다")
+    private TechCategory category;
 }
