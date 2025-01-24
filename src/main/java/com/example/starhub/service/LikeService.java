b2eb5b82d6c5dc0ced8b2b69b1a579b934556eb0
pyounani <yndbsk9372@gmail.com>
2025-01-24 10:37:31 +0900

    [Feat] 좋아요에 대해 기본 틀 작성 #35

---
+++ src/main/java/com/example/starhub/service/LikeService.java
@@ -0,0 +1,14 @@
+package com.example.starhub.service;
+
+import com.example.starhub.repository.LikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LikeService {
+
+    private final LikeRepository likeRepository;
+}
