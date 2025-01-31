576daf825ac9648d13c6ecab3479f22257aa6e5a
pyounani <yndbsk9372@gmail.com>
2025-01-31 15:37:47 +0900

    [Feat] 마이페이지 관련 컨트롤러 설계 #41

--- src/main/java/com/example/starhub/controller/UserController.java
+++ src/main/java/com/example/starhub/controller/UserController.java
@@ -99,6 +99,65 @@ public class UserController implements UserControllerDocs {
                 .body(new ResponseDto<>(ResponseCode.SUCCESS_REISSUE_TOKEN, null));
     }
 
+    /**
+     * 마이페이지 정보 불러오기 - 사용자 정보
+     */
+    @GetMapping("/mypage/users")
+    public ResponseEntity<ResponseDto> getUserProfile() {
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_PROFILE.getStatus().value())
+                .body(new ResponseDto<>(ResponseCode.SUCCESS_GET_PROFILE, null));
+    }
+
+    /**
+     * 마이페이지 정보 불러오기 - 모임 목록
+     */
+    @GetMapping("/mypage/meetings")
+    public ResponseEntity<ResponseDto> getUserMeetings() {
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_MEETINGS.getStatus().value())
+                .body(new ResponseDto<>(ResponseCode.SUCCESS_GET_MEETINGS, null));
+    }
+
+    /**
+     * 마이페이지 정보 수정하기
+     */
+    @PutMapping("/mypage/users")
+    public ResponseEntity<ResponseDto> updateUserProfile() {
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_UPDATE_PROFILE.getStatus().value())
+                .body(new ResponseDto<>(ResponseCode.SUCCESS_UPDATE_PROFILE, null));
+    }
+
+    /**
+     * 내가 작성한 글 목록 불러오기
+     */
+    @GetMapping("/mypage/meetings/created")
+    public ResponseEntity<ResponseDto> getCreatedMeetings() {
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_CREATED_MEETINGS.getStatus().value())
+                .body(new ResponseDto<>(ResponseCode.SUCCESS_GET_CREATED_MEETINGS, null));
+    }
+
+    /**
+     * 내가 관심있는 글 목록 불러오기
+     */
+    @GetMapping("/mypage/meetings/liked")
+    public ResponseEntity<ResponseDto> getLikedMeetings() {
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_LIKED_MEETINGS.getStatus().value())
+                .body(new ResponseDto<>(ResponseCode.SUCCESS_GET_LIKED_MEETINGS, null));
+    }
+
+    /**
+     * 내가 참여한 모임 글 목록 불러오기
+     */
+    @GetMapping("/mypage/meetings/applied")
+    public ResponseEntity<ResponseDto> getAppliedMeetings() {
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS_GET_APPLIED_MEETINGS.getStatus().value())
+                .body(new ResponseDto<>(ResponseCode.SUCCESS_GET_APPLIED_MEETINGS, null));
+    }
 
 
     private Cookie createCookie(String key, String value) {
